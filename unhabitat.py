46665a677e3a8ad5b06c41c803858118fe2b1328
Briar <briar.j.mills@gmail.com>
2024-02-28 15:58:35 -0500

    Make dates more flexible

--- unhabitat.py
+++ unhabitat.py
@@ -10,7 +10,7 @@ import logging
 
 from hdx.data.dataset import Dataset
 from hdx.location.country import Country
-from hdx.utilities.dictandlist import dict_of_lists_add, write_list_to_csv
+from hdx.utilities.dictandlist import dict_of_lists_add, dict_of_sets_add, write_list_to_csv
 from hdx.data.resource import Resource
 from os.path import join
 from pandas import DataFrame, ExcelWriter
@@ -44,27 +44,36 @@ class UNHabitat:
 
             for row in iterator:
                 row = {key: row[key] for key in row if key in dataset_info["headers"]}
-                year = row[dataset_info["date_header"]]
                 country_name = row[dataset_info["country_header"]]
-                country_iso3 = Country.get_iso3_country_code(country_name)
+                if not country_name:
+                    continue
+                iso3 = Country.get_iso3_country_code(country_name)
 
-                if country_iso3 in self.configuration["countries"]:
-                    dict_of_lists_add(self.data, f"{dataset_name}_{country_iso3}", row)
-                    dict_of_lists_add(self.dates, f"{dataset_name}_{country_iso3}", year)
+                if iso3 in self.configuration["countries"]:
+                    dict_of_lists_add(self.data, f"{dataset_name}_{iso3}", row)
+                    if dataset_info.get("date_header"):
+                        dict_of_sets_add(self.dates, f"{dataset_name}_{iso3}", row[dataset_info["date_header"]])
+                    else:
+                        dict_of_sets_add(self.dates, f"{dataset_name}_{iso3}", dataset_info["date_min"])
+                        dict_of_sets_add(self.dates, f"{dataset_name}_{iso3}", dataset_info["date_max"])
 
                 dict_of_lists_add(self.data, f"{dataset_name}_world", row)
-                dict_of_lists_add(self.dates, f"{dataset_name}_world", year)
+                if dataset_info.get("date_header"):
+                    dict_of_sets_add(self.dates, f"{dataset_name}_world", row[dataset_info["date_header"]])
+                else:
+                    dict_of_sets_add(self.dates, f"{dataset_name}_world", dataset_info["date_min"])
+                    dict_of_sets_add(self.dates, f"{dataset_name}_world", dataset_info["date_max"])
 
         return [{"name": dataset_name} for dataset_name in sorted(self.data)]
 
     def generate_dataset(self, dataset_name):
         dataset_info = self.configuration["datasets"][dataset_name.rsplit("_", 1)[0]]
-        country_iso3 = dataset_name.rsplit("_", 1)[1]
-        if country_iso3 == "world":
+        iso3 = dataset_name.rsplit("_", 1)[1]
+        if iso3 == "world":
             country_name = "world"
             title = dataset_info["title"].replace("country - ", "")
         else:
-            country_name = Country.get_country_name_from_iso3(country_iso3)
+            country_name = Country.get_country_name_from_iso3(iso3)
             title = dataset_info["title"].replace("country", country_name)
         dataset = Dataset({"name": slugify(dataset_name), "title": title})
         dataset["notes"] = dataset_info["notes"]
@@ -74,20 +83,20 @@ class UNHabitat:
             min(self.dates[dataset_name]),
             max(self.dates[dataset_name]),
         )
-        if country_iso3 == "world":
+        if iso3 == "world":
             dataset.add_other_location(country_name)
         else:
             dataset.add_country_location(country_name)
         dataset.add_tags(dataset_info["tags"])
 
         rows = self.data[dataset_name]
-        self.generate_resource(dataset, country_iso3, rows, dataset_info, "csv")
-        self.generate_resource(dataset, country_iso3, rows, dataset_info, "xlsx")
+        self.generate_resource(dataset, iso3, rows, dataset_info, "csv")
+        self.generate_resource(dataset, iso3, rows, dataset_info, "xlsx")
         return dataset
 
-    def generate_resource(self, dataset, country_iso3, rows, dataset_info, file_format):
+    def generate_resource(self, dataset, iso3, rows, dataset_info, file_format):
         headers = list(rows[0].keys())
-        filename = f"{dataset_info['filename']}_{country_iso3}"
+        filename = f"{dataset_info['filename']}_{iso3}"
         filepath = join(self.folder, f"{filename}.{file_format}")
         resource = Resource(
             {
@@ -100,10 +109,10 @@ class UNHabitat:
         if file_format == "xlsx":
             df = DataFrame(rows)
             writer = ExcelWriter(filepath, engine="xlsxwriter")
-            df.to_excel(writer, sheet_name=filename, index=False)
+            df.to_excel(writer, sheet_name=filename[:24], index=False)
             if dataset_info["value_header"]:
                 workbook = writer.book
-                worksheet = writer.sheets[filename]
+                worksheet = writer.sheets[filename[:24]]
                 num_format = workbook.add_format({"num_format": "0.0"})
                 for header in dataset_info["value_header"]:
                     worksheet.set_column(headers.index(header), headers.index(header), None, num_format)

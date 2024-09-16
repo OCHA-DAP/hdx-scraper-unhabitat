9fde30cf33c20c8a63cf0be307795cdc78b32af6
Briar <briar.j.mills@gmail.com>
2024-09-16 15:33:36 -0400

    Update tests

--- tests/test_unhabitat.py
+++ tests/test_unhabitat.py
@@ -25,7 +25,9 @@ class TestUNHabitat:
         "title": "Afghanistan - Open spaces and green areas",
         "notes": "Data on a) Average share of urban areas allocated to streets and open public spaces, and b) Share of "
                  "urban population with convenient access to an open public space (defined as share of urban "
-                 "population within 400 meters walking distance along the street network to an open public space).",
+                 "population within 400 meters walking distance along the street network to an open public space). \n"
+                 "Data on a) Average share of cities/urban areas in green areas (%), and b) Green area per capita "
+                 "(m2/person) for the periods 1990, 2000, 2010 and 2020.",
         "groups": [{"name": "afg"}],
         "tags": [
             {"name": "urban", "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1"},
@@ -39,7 +41,35 @@ class TestUNHabitat:
         "dataset_date": "[2020-01-01T00:00:00 TO 2020-12-31T23:59:59]",
         "license_id": "hdx-pddl",
         "methodology": "Other",
-        "methodology_other": "See methodology at https://data.unhabitat.org/pages/guidance",
+        "methodology_other": "The data referenced herein is calculated using urban boundaries defined using the Degree "
+                             "of Urbanization approach to defining cities and urban areas, which may be larger or "
+                             "smaller than the official municipality boundaries.  Within each city/urban area, the "
+                             "green areas are extracted using satellite imagery analysis for four time periods 1990, "
+                             "2000, 2010 and 2020 based on the Normalized Difference Vegetation Index (NDVI), which "
+                             "assesses the level of greenness from satellite imagery. In this analysis, green areas "
+                             "are defined as parts of the city that are green for most parts of the year, and include "
+                             "individual trees, forests or forested areas, shrubs, perennial grasses and such other "
+                             "types of long-term vegetation. Waterbodies are not considered as green areas in this "
+                             "assessment. Since the stability the input data can be affected by climatic conditions "
+                             "such as long-term drought or rain, a median image was created over a three-year period, "
+                             "reducing thousands of images available for each point(pixel) to the most stable pixel. "
+                             "The median was used as opposed to the mean, because the median is less influenced by "
+                             "extremes (in this case extreme rains or drought). \n"
+                             "Unlike majority of the existing datasets which use basic NDVI thresholds, the most "
+                             "common of which include values of -1 to <0.2 for non-vegetated pixels, >=0.2 – 0.5 for "
+                             "low vegetation and >0.5 for high vegetation , the premise of this new analysis was that "
+                             "each city is unique, and using a constant NDVI threshold would work to the disadvantage "
+                             "of cities in drier regions. As such, NDVI thresholds were manually defined for each city "
+                             "per year by GIS experts at UN-Habitat. The analysis framework – including selection and "
+                             "mosaicking of images from the Landsat and Sentinel missions, pre-processing and NDVI "
+                             "analysis and thresholding was based on the Google Earth Engine platform. \n"
+                             "The results from this analysis were then used to calculate two indicators: a) the "
+                             "average share of green area in city/urban area (percentage) and  b) the green are per "
+                             "capita (m2 per person). Population data used to calculate the green area per capita is "
+                             "sourced from GHS-Pop for 1990, 2000, 2010 and 2020. \n"
+                             "The calculation of global and regional averages for the indicator on share of green area "
+                             "in city/urban areas are based on simple averages, while those on green area per capita "
+                             "are based on population weighted averages per analysis year.",
         "caveats": "Read more at https://unhabitat.org/knowledge/data-and-analytics",
         "subnational": "1",
         "private": False,
@@ -105,6 +135,7 @@ class TestUNHabitat:
                 dataset.update_from_yaml()
                 assert dataset == self.dataset_open_spaces
                 resources = dataset.get_resources()
+                assert len(resources) == 4
                 assert resources[0] == self.resource_open_spaces_csv
                 file = "SDG_11-7-1_AFG.csv"
                 assert_files_same(join("tests", "fixtures", file), join(folder, file))
